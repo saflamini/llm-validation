c514c9ddb51ca8b25b65d889d5d2f95e92aa5c4f
Sam Flamini <saflamini@gmail.com>
2023-10-26 20:03:15 -0700

    chunks paras

--- python/embeddings_citations.py
+++ python/embeddings_citations.py
@@ -7,10 +7,10 @@ import requests
 nltk.download('punkt')
 
 # Replace with your API token
-aai.settings.api_key = f""
+aai.settings.api_key = ""
 lemur_endpoint = "https://api.assemblyai.com/lemur/v3/generate/task"
 headers = {
-    "Authorization": "KEY"
+    "Authorization": ""
 }
 
 # URL of the file to transcribe
@@ -31,24 +31,54 @@ sentences = []
 for sentence in transcript.get_sentences():
     sentences.append(sentence.text)
 
+def sliding_window(sentences, window_size=3):
+    """
+    Splits sentences into chunks using a sliding window approach.
+    
+    Args:
+    - sentences (list of str): List of sentences from the transcript.
+    - window_size (int): Number of sentences per chunk.
+    
+    Returns:
+    - list of str: Chunks of sentences.
+    """
+    chunks = []
+    for i in range(0, len(sentences) - window_size + 1):
+        chunk = " ".join(sentences[i:i+window_size])
+        chunks.append(chunk)
+    return chunks
+
+
 # Load the pre-trained sentence transformer model
 # Note - the first time you do this, it will take some time to download the model
 # We are downloading the model locally
 # Why all-MiniLM-L6-v2? it's near the top of the leaderboard for semantic similarity search and is 5x faster than the largest model
 # It's also a good model for semantic similarity because it's been trained on the STSB benchmark dataset
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+# model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+model = SentenceTransformer("infgrad/stella-base-en-v2")
 
 # Embed each sentence in sentences
 sentence_embeddings = model.encode(sentences)
+paragraph_embeddings = model.encode(paragraphs)
+# Create chunks of 3 sentences using the sliding window approach
+sentence_chunks = sliding_window(sentences)
+# Embed each chunk
+chunk_embeddings = model.encode(sentence_chunks)
 
-lemur_summary = transcript.lemur.summarize(
-    context="Please summarize this transcript as though I were a 10 year old",
-    answer_format="paragaraphs"
-)
+def compare_against_all_granularities(summary_embedding, embeddings_list, k):
+    """
+    Compare the summary embedding against a list of embeddings and return the top_k similar items' indices and scores.
+    """
+    similarities = cosine_similarity([summary_embedding], embeddings_list)[0]
+    top_k_indices = similarities.argsort()[-k:][::-1]
+    return top_k_indices, similarities[top_k_indices]
+
+
+lemur_summary = "Wildfires in Canada are making the air dirty in many places in the US. Smoke from the fires is traveling through the sky and making it hard to breathe in places like New York and Baltimore. The smoke has tiny pieces in it that can get inside your lungs if you breathe them. This can make you sick, especially kids and older adults. The pieces in the smoke are much more than normal and that's why the air is unhealthy. More people could get sick until the weather changes and moves the smoke away. Fires might happen more often in the future because of climate change, so dirty air could affect more places."
 
 # Split the summary into sentences
 # summary_sentences = lemur_summary.response.split(". ")
-summary_sentences = nltk.sent_tokenize(lemur_summary.response)
+summary_sentences = nltk.sent_tokenize(lemur_summary)
 
 # Embed each sentence in the summary and store a mapping between the sentence text and the embedding
 summary_embeddings = {}
@@ -56,42 +86,47 @@ for sentence in summary_sentences:
     embedding = model.encode([sentence])[0]
     summary_embeddings[sentence] = embedding
 
-k = 3 # top k similar sentences to retrieve
+k = 3  # top k similar items to retrieve
 
 new_summary = ""
+filtered_sentences = []
+
+sentence_threshold = 0.80 # Typically set higher than paragraph_threshold
+paragraph_threshold = 0.73 # Typically set lower than sentence_threshold
+chunk_threshold = 0.78  # This could be set between the sentence and paragraph thresholds
+
 for summary_sentence, summary_embedding in summary_embeddings.items():
-    similarities = cosine_similarity([summary_embedding], sentence_embeddings)[0]
-    top_k_index = similarities.argsort()[-1]
-    top_k_sentence = sentences[top_k_index]
-    top_k_similarity = similarities[top_k_index]
-    print(f"SUMMARY SENTENCE: {summary_sentence} \nMOST SIMILAR SENTENCE: {top_k_sentence} (similarity score: {top_k_similarity})\n")
+    # Compare the summary sentence against all three granularities
+    top_k_sentence_indices, top_k_sentence_similarities = compare_against_all_granularities(summary_embedding, sentence_embeddings, k)
+    top_k_paragraph_indices, top_k_paragraph_similarities = compare_against_all_granularities(summary_embedding, paragraph_embeddings, k)
+    top_k_chunk_indices, top_k_chunk_similarities = compare_against_all_granularities(summary_embedding, chunk_embeddings, k)
+    
+    # Extract the most similar item from each granularity
+    most_similar_sentence = sentences[top_k_sentence_indices[0]]
+    most_similar_paragraph = paragraphs[top_k_paragraph_indices[0]]
+    most_similar_chunk = sentence_chunks[top_k_chunk_indices[0]]
+    
+    # Check if the similarity scores cross their respective thresholds
+    sentence_pass = any(sim >= sentence_threshold for sim in top_k_sentence_similarities)
+    paragraph_pass = any(sim >= paragraph_threshold for sim in top_k_paragraph_similarities)
+    chunk_pass = any(sim >= chunk_threshold for sim in top_k_chunk_similarities)
+    
+    # If any of the granularities pass their threshold, include the summary sentence
+    if sentence_pass or paragraph_pass or chunk_pass:
+        new_summary += summary_sentence + " "
+    else:
+        filtered_sentences.append(summary_sentence)
+    
+    # Log the most similar items with their scores
+    print("********************************")
+    print(f"SUMMARY SENTENCE: {summary_sentence}")
+    print(f"Most similar sentence: {most_similar_sentence} (score: {top_k_sentence_similarities[0]})")
+    print(f"Most similar paragraph: {most_similar_paragraph} (score: {top_k_paragraph_similarities[0]})")
+    print(f"Most similar chunk: {most_similar_chunk} (score: {top_k_chunk_similarities[0]})")
 
-def rebuild_summary_without_duplicates(summary_embeddings, sentence_embeddings, threshold=0.6, k=3):
-    """
-    Rebuilds the summary by filtering out potentially hallucinated sentences.
-    """
-    new_summary = ""
-    added_sentences = set()
-    filtered_sentences = []
-
-    for summary_sentence, summary_embedding in summary_embeddings.items():
-        similarities = cosine_similarity([summary_embedding], sentence_embeddings)[0]
-        top_k_indices = similarities.argsort()[-k:][::-1]
-        top_k_similarities = [similarities[i] for i in top_k_indices]
-        if any(sim >= threshold for sim in top_k_similarities) and summary_sentence not in added_sentences:
-            new_summary += summary_sentence + " "
-            added_sentences.add(summary_sentence)
-        else:
-            filtered_sentences.append(summary_sentence)
-
-    return new_summary.strip(), filtered_sentences
-
-# Using the provided embeddings and logic to rebuild the summary without duplicates
-new_summary, filtered_sentences = rebuild_summary_without_duplicates(summary_embeddings, sentence_embeddings)
 print("***************************")
 print("NEW SUMMARY OUTPUT")
 print(new_summary)
 print("***************************")
 print("FILTERED SENTENCES")
 print(filtered_sentences)
-
